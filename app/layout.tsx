af3ead1ad7b320126e246f7fcb77f068be031c8a
Terry Cheng <cyr062599@gmail.com>
2025-06-28 17:41:05 +0930

    Add Umami analytics script to RootLayout: Integrate tracking script for website analytics in the layout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,11 @@ export default function RootLayout({
         </div>
         <Toaster richColors />
       </body>
+      <Script
+        defer
+        src="https://umami.lemontea.xyz/script.js"
+        data-website-id="7a89e33e-1d8b-4f4a-825b-439689fa6de7"
+      />
     </html>
   );
 }
