896d304dc0ced44621ae949e2c184882df9c05fc
Terry Cheng <cyr062599@gmail.com>
2025-07-13 15:41:22 +0930

    Update hostname in next.config.ts: Change image source from '*.r2.dev' to 'image.artistarray.com' for improved image loading configuration.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.r2.dev",
+        hostname: "image.artistarray.com",
       },
     ],
   },
